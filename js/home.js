c42db9640d6b787ad90b464469fcbb55856f6a22
Tetteh Yaw Precious <precioustetteh38@gmail.com>
2021-07-22 08:03:48 -0700

    update on GoldYardRecord slider

---
+++ js/home.js
@@ -0,0 +1,16 @@
+"use strict";
+//Slider
+//selecting elements
+const leftBtn = document.querySelector(".--left");
+const rightBtn = document.querySelector(".--right");
+let item = document.querySelector(".--item");
+let slider = document.querySelector(".slider");
+
+leftBtn.addEventListener("click", () => {
+  slider.scrollLeft -= 420;
+  console.log("fd");
+});
+rightBtn.addEventListener("click", () => {
+  slider.scrollLeft += 420;
+  console.log("fd");
+});
